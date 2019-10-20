dcdd43f199515b352a6634d4bdac71abb2fc1a20
Rebecca107 <54847956+Rebecca107@users.noreply.github.com>
2019-10-20 20:18:23 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    include_once 'core/init.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include_once 'includes/head.php';
+    ?>
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>No Hesi Apparel</title>
+</head>
+<body>
+    <?php
+        include_once 'includes/header.php';
+    ?>
+    <section>
+        <div class="hero">
+            <div class="overlay">
+                <div>
+                    <div class="title">
+                        <h2>welcome to <span>no hesi</span> apparel</h2>
+                    </div>
+                    <div class="anchor">
+                        <a href="shop.php"><i class="fa-shopping-cart"></i> start shopping</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php
+        include_once 'includes/scripts.php';
+    ?>
+</body>
+</html>
